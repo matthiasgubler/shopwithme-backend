64d58b59a2572a855035793200aa342f0cfc6e87
colomdan <colomdan@students.zhaw.ch>
2020-04-27 23:23:48 +0200

    quickfix (#28)

--- src/main/java/ch/zhaw/swm/wall/config/AppConfig.java
+++ src/main/java/ch/zhaw/swm/wall/config/AppConfig.java
@@ -1,11 +1,9 @@
 package ch.zhaw.swm.wall.config;
 
 import ch.zhaw.swm.wall.interceptor.AuthenticationInterceptor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
 public class AppConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
