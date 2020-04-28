f9d9abf833fb2343a3a241f0ac3b0d9746539e4b
Menchu Bürki <buerkcar@students.zhaw.ch>
2020-04-28 09:16:25 +0200

    SWM-134: Move endpoint get posts by topic id from post to topic controller (#26)

--- src/main/java/ch/zhaw/swm/wall/controller/PostController.java
+++ src/main/java/ch/zhaw/swm/wall/controller/PostController.java
@@ -2,16 +2,12 @@ package ch.zhaw.swm.wall.controller;
 
 import ch.zhaw.swm.wall.model.post.Comment;
 import ch.zhaw.swm.wall.model.post.CommentCreation;
-import ch.zhaw.swm.wall.model.post.Post;
 import ch.zhaw.swm.wall.model.post.Location;
-import ch.zhaw.swm.wall.model.post.PostStructure;
+import ch.zhaw.swm.wall.model.post.Post;
 import ch.zhaw.swm.wall.services.post.PostService;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class PostController extends BasicController {
@@ -19,7 +15,6 @@ public class PostController extends BasicController {
     private final PostService postService;
     private static final String URI_POSTS = "/posts";
     private static final String URI_COMMENTS = "/comments";
-    private static final String URI_TOPIC = "/topics";
     private static final String URI_LOCATION = "/locations";
     private static final String ID = "/{id}";
 
@@ -46,12 +41,6 @@ public class PostController extends BasicController {
     @PostMapping(URI_POSTS + URI_LOCATION)
     public Location newLocation(@RequestBody Location location) {
         return postService.createLocationPost(location);
-    }
-
-    @GetMapping(URI_POSTS + URI_TOPIC + ID)
-    @Operation(description = "Get posts by topic id ordered by create datetime.")
-    public List<PostStructure> getPostsByTopicIdStructured(@PathVariable String id) {
-        return postService.findAllPostsByTopicIdStructured(id);
     }
 
 }
--- src/main/java/ch/zhaw/swm/wall/controller/TopicsController.java
+++ src/main/java/ch/zhaw/swm/wall/controller/TopicsController.java
@@ -1,8 +1,11 @@
 package ch.zhaw.swm.wall.controller;
 
+import ch.zhaw.swm.wall.model.post.PostStructure;
 import ch.zhaw.swm.wall.model.topic.Topic;
 import ch.zhaw.swm.wall.model.topic.TopicPatch;
+import ch.zhaw.swm.wall.services.post.PostService;
 import ch.zhaw.swm.wall.services.topic.TopicService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,36 +14,49 @@ import java.util.List;
 
 @RestController
 public class TopicsController extends BasicController {
+
     private final TopicService topicService;
+    private final PostService postService;
+
+    private static final String URI_TOPICS = "/topics";
+    private static final String URI_POSTS = "/posts";
+    private static final String ID = "/{id}";
 
     @Autowired
-    public TopicsController(TopicService topicService) {
+    public TopicsController(TopicService topicService, PostService postService) {
         this.topicService = topicService;
+        this.postService = postService;
     }
 
-    @PostMapping("/topics")
+    @PostMapping(URI_TOPICS)
     public Topic newTopic(@RequestBody Topic newTopic) {
         return topicService.create(newTopic);
     }
 
-    @GetMapping("/topics")
+    @GetMapping(URI_TOPICS)
     public List<Topic> all() {
         return topicService.findAll();
     }
 
-    @PatchMapping("/topics/{id}")
+    @PatchMapping(URI_TOPICS + ID)
     public Topic patch(@RequestBody TopicPatch topicPatch, @PathVariable String id) {
         return this.topicService.patch(topicPatch, id);
     }
 
-    @DeleteMapping("/topics/{id}")
+    @DeleteMapping(URI_TOPICS + ID)
     public void delete(@PathVariable String id) {
         this.topicService.delete(id);
     }
 
-    @GetMapping("/topics/{id}")
+    @GetMapping(URI_TOPICS + ID)
     public ResponseEntity<Topic> one(@PathVariable String id) {
         return handleSingleResourceResponse(topicService.findById(id));
+    }
+
+    @GetMapping(URI_TOPICS + ID + URI_POSTS)
+    @Operation(description = "Get posts by topic id ordered by create datetime.")
+    public List<PostStructure> getPostsByTopicIdStructured(@PathVariable String id) {
+        return postService.findAllPostsByTopicIdStructured(id);
     }
 
 }
