bd4ff5dbcd6b9fedad22fb61153479e38dd192f0
colomdan <colomdan@students.zhaw.ch>
2020-05-03 13:51:00 +0200

    SWM-114 reactivate authentication-inteceptor (#30)

--- src/main/java/ch/zhaw/swm/wall/config/AppConfig.java
+++ src/main/java/ch/zhaw/swm/wall/config/AppConfig.java
@@ -1,12 +1,14 @@
 package ch.zhaw.swm.wall.config;
 
 import ch.zhaw.swm.wall.interceptor.AuthenticationInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class AppConfig extends WebMvcConfigurationSupport {
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthenticationInterceptor());
     }
 }
